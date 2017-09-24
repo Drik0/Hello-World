f3e6ca5c938e629401fc933a95dc55c456416d22
Gerson <gerson.lcc@hotmail.com>
2017-09-24 11:29:12 +0100

    Implementation of UIaction

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -17,14 +17,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         bgImageView.isHidden = true
         logoImageView.isHidden = true
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func welcomeBtnPressed(_ sender: Any) {
+        bgImageView.isHidden = false
+        logoImageView.isHidden = false
+    }
+    
 }
 
